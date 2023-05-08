c863c2e757b4fedac03c417321ed3602ef8ad852
Nguyen Ngo Cao Cuong <nguyenngocaocuong@gmail.com>
2023-05-08 13:54:13 +0700

    done config router dom v6

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { ColorModeContext, useMode } from "./theme";
 import { Box, ThemeProvider } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
 function App() {
   const [theme, colorMode] = useMode();
   return (
     <ColorModeContext.Provider value={colorMode}>
-        <ThemeProvider theme={theme}>
-            <Box>Welcome</Box>
-        </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Box>Welcome</Box>
+        </BrowserRouter>
+      </ThemeProvider>
     </ColorModeContext.Provider>
-  )
+  );
 }
 
 export default App;
