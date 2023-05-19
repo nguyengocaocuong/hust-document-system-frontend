441b11112f538d5293a170e31e044890b1112fd9
Nguyen Ngo Cao Cuong <nguyenngocaocuong@gmail.com>
2023-05-19 10:54:39 +0700

    change color for home button in sidebar

--- src/containers/sidebar/SidebarHomeItem.jsx
+++ src/containers/sidebar/SidebarHomeItem.jsx
@@ -2,10 +2,11 @@ import { Box, Typography } from "@mui/material";
 import { useProSidebar } from "react-pro-sidebar";
 import React from "react";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function SidebarHomeItem() {
   const { collapsed } = useProSidebar();
+  const location = useLocation();
   return (
     <Box
       width={"100%"}
@@ -20,20 +21,22 @@ function SidebarHomeItem() {
         alignItems={"center"}
         sx={{
           background:
-            "linear-gradient(90deg, rgba(255,0,4,1) 0%, rgba(255,82,82,0.8856792717086834) 0%, rgba(255,0,0,1) 100%)",
+            location.pathname === "/"
+              ? "linear-gradient(90deg, rgba(255,0,4,1) 0%, rgba(255,82,82,0.8856792717086834) 0%, rgba(255,0,0,1) 100%)"
+              : "gray",
           width: "90%",
           height: "50px",
           borderRadius: "25px",
         }}
       >
-        <HomeOutlinedIcon
-          style={{
-            fontSize: "30px",
-            color: "white",
-          }}
-        />
-        {!collapsed && (
-          <Link to="/" style={{ textDecoration: "none" }}>
+        <Link to="/" style={{ textDecoration: "none", alignItems:'center', display:'flex' }}>
+          <HomeOutlinedIcon
+            style={{
+              fontSize: "30px",
+              color: "white",
+            }}
+          />
+          {!collapsed && (
             <Typography
               variant="h4"
               color={"white"}
@@ -42,8 +45,8 @@ function SidebarHomeItem() {
             >
               Trang chủ
             </Typography>
-          </Link>
-        )}
+          )}
+        </Link>
       </Box>
     </Box>
   );
